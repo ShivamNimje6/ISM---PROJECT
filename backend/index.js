4ce14772b464b5fca5d9131d293a062ce6822a65
ShivamNimje6 <shivamnimje6@gmail.com>
2026-04-06 21:25:01 +0530

    Add .gitignore and remove tracked node_modules before GitHub push

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,82 @@
 const express = require("express");
-
 const app = express();
-//database
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-app.listen(5000, () => {
-  console.log("app is running on port 5000");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+
+// database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database is connected successfully!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// MIDDLEWARES
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
+
+// image upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.img);
+  },
+});
+
+// File filter to allow only .jpg and .jpeg files
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Only .jpg and .jpeg formats are allowed!"));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+}).single("file");
+
+app.post("/api/upload", (req, res) => {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      return res
+        .status(500)
+        .json({ message: "Multer error occurred: " + err.message });
+    } else if (err) {
+      return res.status(400).json({ message: err.message });
+    }
+    res.status(200).json("Image has been uploaded successfully!");
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log("app is running on port " + process.env.PORT);
 });
